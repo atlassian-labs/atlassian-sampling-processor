52a7f5a92fe6362b3d14ef5f69d77307e0226f21
James Moessis <jmoessis@atlassian.com>
2025-04-29 09:24:04 +1000

    sync with upstream. This upgrades to collector version 0.122

--- pkg/processor/atlassiansamplingprocessor/internal/metadata/generated_telemetry.go
+++ pkg/processor/atlassiansamplingprocessor/internal/metadata/generated_telemetry.go
@@ -4,11 +4,10 @@ package metadata
 
 import (
 	"errors"
+	"sync"
 
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/config/configtelemetry"
 	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/noop"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -24,6 +23,8 @@ func Tracer(settings component.TelemetrySettings) trace.Tracer {
 // as defined in metadata and user config.
 type TelemetryBuilder struct {
 	meter                                                        metric.Meter
+	mu                                                           sync.Mutex
+	registrations                                                []metric.Registration
 	ProcessorAtlassianSamplingCacheReads                         metric.Int64Counter
 	ProcessorAtlassianSamplingChanBlockingTime                   metric.Int64Histogram
 	ProcessorAtlassianSamplingDecisionEvictionTime               metric.Float64Gauge
@@ -47,6 +48,15 @@ func (tbof telemetryBuilderOptionFunc) apply(mb *TelemetryBuilder) {
 	tbof(mb)
 }
 
+// Shutdown unregister all registered callbacks for async instruments.
+func (builder *TelemetryBuilder) Shutdown() {
+	builder.mu.Lock()
+	defer builder.mu.Unlock()
+	for _, reg := range builder.registrations {
+		reg.Unregister()
+	}
+}
+
 // NewTelemetryBuilder provides a struct with methods to update all internal telemetry
 // for a component
 func NewTelemetryBuilder(settings component.TelemetrySettings, options ...TelemetryBuilderOption) (*TelemetryBuilder, error) {
@@ -56,73 +66,66 @@ func NewTelemetryBuilder(settings component.TelemetrySettings, options ...Teleme
 	}
 	builder.meter = Meter(settings)
 	var err, errs error
-	builder.ProcessorAtlassianSamplingCacheReads, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Counter(
+	builder.ProcessorAtlassianSamplingCacheReads, err = builder.meter.Int64Counter(
 		"otelcol_processor_atlassian_sampling_cache_reads",
 		metric.WithDescription("Amount of times a cache was read from"),
 		metric.WithUnit("{accesses}"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingChanBlockingTime, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Histogram(
+	builder.ProcessorAtlassianSamplingChanBlockingTime, err = builder.meter.Int64Histogram(
 		"otelcol_processor_atlassian_sampling_chan_blocking_time",
 		metric.WithDescription("Amount of time spent blocking on the chan send in ConsumeTraces()"),
 		metric.WithUnit("ns"),
 		metric.WithExplicitBucketBoundaries([]float64{50000, 100000, 500000, 1e+06, 5e+06, 1e+07, 5e+07, 1e+08, 1e+09, 1.5e+09}...),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingDecisionEvictionTime, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Float64Gauge(
+	builder.ProcessorAtlassianSamplingDecisionEvictionTime, err = builder.meter.Float64Gauge(
 		"otelcol_processor_atlassian_sampling_decision_eviction_time",
 		metric.WithDescription("Time that a trace ID spent in the decision cache before it was evicted"),
 		metric.WithUnit("s"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingInternalErrorDroppedSpans, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Counter(
+	builder.ProcessorAtlassianSamplingInternalErrorDroppedSpans, err = builder.meter.Int64Counter(
 		"otelcol_processor_atlassian_sampling_internal_error_dropped_spans",
 		metric.WithDescription("Number of spans that have been dropped due to an internal error"),
 		metric.WithUnit("{spans}"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingOverlyEagerLonelyRootSpanDecisions, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Counter(
+	builder.ProcessorAtlassianSamplingOverlyEagerLonelyRootSpanDecisions, err = builder.meter.Int64Counter(
 		"otelcol_processor_atlassian_sampling_overly_eager_lonely_root_span_decisions",
 		metric.WithDescription("Number of spans that have been aggressively sampled out by root span policy"),
 		metric.WithUnit("{spans}"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingPolicyDecisions, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Counter(
+	builder.ProcessorAtlassianSamplingPolicyDecisions, err = builder.meter.Int64Counter(
 		"otelcol_processor_atlassian_sampling_policy_decisions",
 		metric.WithDescription("Sampling decisions made specifying policy and decision."),
 		metric.WithUnit("{decisions}"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingPrimaryCacheSize, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Gauge(
+	builder.ProcessorAtlassianSamplingPrimaryCacheSize, err = builder.meter.Int64Gauge(
 		"otelcol_processor_atlassian_sampling_primary_cache_size",
 		metric.WithDescription("Size on the primary cache"),
 		metric.WithUnit("{traces}"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingTraceEvictionTime, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Float64Gauge(
+	builder.ProcessorAtlassianSamplingTraceEvictionTime, err = builder.meter.Float64Gauge(
 		"otelcol_processor_atlassian_sampling_trace_eviction_time",
 		metric.WithDescription("Time that a non-sampled trace was kept in memory from arrival to being evicted"),
 		metric.WithUnit("s"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingTracesNotSampled, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Counter(
+	builder.ProcessorAtlassianSamplingTracesNotSampled, err = builder.meter.Int64Counter(
 		"otelcol_processor_atlassian_sampling_traces_not_sampled",
 		metric.WithDescription("Number of traces dropped and not sampled"),
 		metric.WithUnit("{traces}"),
 	)
 	errs = errors.Join(errs, err)
-	builder.ProcessorAtlassianSamplingTracesSampled, err = getLeveledMeter(builder.meter, configtelemetry.LevelBasic, settings.MetricsLevel).Int64Counter(
+	builder.ProcessorAtlassianSamplingTracesSampled, err = builder.meter.Int64Counter(
 		"otelcol_processor_atlassian_sampling_traces_sampled",
 		metric.WithDescription("Number of traces sampled"),
 		metric.WithUnit("{traces}"),
 	)
 	errs = errors.Join(errs, err)
 	return &builder, errs
-}
-
-func getLeveledMeter(meter metric.Meter, cfgLevel, srvLevel configtelemetry.Level) metric.Meter {
-	if cfgLevel <= srvLevel {
-		return meter
-	}
-	return noop.Meter{}
 }
